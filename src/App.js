a30404d487a277e1c47e8077f9f8fb60457dcd79
SamerSiam <ssiam05@gmail.com>
2021-12-24 14:04:10 +0200

    init setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react'
+import CRUD from '../src/API/Data'
+
+class App extends Component {
+  state={data:[],errorMsg:''}
+
+  async componentDidMount(){
+    try {
+        const {data}=await CRUD.get('Items')
+        this.setState({data:data});
+        } 
+        catch (err) {
+            this.setState ({errorMsg:err.message})
+        }
+    }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
